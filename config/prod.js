6df3300ab83c22ee0888106103b6524380cc0023
satheeshkumar-a <satheeshkumara@hotmail.com>
2021-11-27 11:32:34 +0530

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+// "519127875449-per8k7j7cs3vkeukvmp3a441ol1qnh9a.apps.googleusercontent.com"
+// "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"
+//  "mongodb+srv://satheeshprod:Asdf123@emailyprod.3bysx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//  "asdfghjklqwertyuiop";
