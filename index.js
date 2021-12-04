66bfc18c626a37893d4accdacad518c1878aaf4e
satheeshkumar-a <satheeshkumara@hotmail.com>
2021-12-04 07:56:37 +0530

    path over in index

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  var path = require(path);
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
